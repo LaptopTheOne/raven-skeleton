9ad340d8b7a5594cd8b9bfe51844fb332f8b0e39
LaptopTheOne <moneywithin@yahoo.com>
2023-03-12 15:56:39 +0100

    Added helper functions for managing cookies token data for identity and history.

--- js/helpers.js
+++ js/helpers.js
@@ -1,9 +1,16 @@
 class Helpers {
+  static generateIdentityScopeUrl() {
+    fetch('http://localhost:3000/api/oauth/authorization_token?scope=identity').then(data => {
+      return data.json()
+    }).then(res => {
+      window.location.replace(res);
+    })
+  }
+
   static generateHistoryScopeUrl() {
     fetch('http://localhost:3000/api/oauth/authorization_token?scope=history').then(data => {
       return data.json()
     }).then(res => {
-      console.log(res);
       window.location.replace(res);
     })
   }
@@ -14,3 +21,37 @@ class Helpers {
     }).then(res => console.log(res))
   }
 }
+
+const TOKEN_ID_KEY = 'identity-access-token'
+const TOKEN_HISTORY_KEY = 'history-access-token'
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function checkQueryParams() {
+  let params = (new URL(document.location)).searchParams;
+  return params.has(TOKEN_ID_KEY) || params.has(TOKEN_HISTORY_KEY)
+}
+
+window.onload = function () {
+  let params = (new URL(document.location)).searchParams;
+  if (checkQueryParams()) {
+    if (!getCookie(TOKEN_ID_KEY)) {
+      let token = params.get(TOKEN_ID_KEY);
+      if (token) {
+        document.cookie = `${TOKEN_ID_KEY}=${token}; max-age=604800`;
+      }
+    }
+    if (!getCookie(TOKEN_HISTORY_KEY)) {
+      let token = params.get(TOKEN_HISTORY_KEY);
+      if (token) {
+        document.cookie = `${TOKEN_HISTORY_KEY}=${token}; max-age=604800`;
+      }
+    }
+    // clear query params
+    window.location.replace(document.location.origin);
+  }
+};
