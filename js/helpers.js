b167c56792e796f9a34c4c540362a57b508604ac
LaptopTheOne <moneywithin@yahoo.com>
2023-03-17 17:07:50 +0100

    Updated history-table web component.

--- js/helpers.js
+++ js/helpers.js
@@ -34,12 +34,8 @@ class Helpers {
 
   static getSavedItems() {
     if (getCookie(TOKEN_HISTORY_KEY) && getCookie(USERNAME_KEY)) {
-      fetch(`http://localhost:3001/api/history/get-saved-items?token=${getCookie(TOKEN_HISTORY_KEY)}&username=${getCookie(USERNAME_KEY)}`).then(data => {
-        return data.json()
-      }).then(res => {
-        document.querySelector('#raven-history-saved-items').savedItems = res;
-        console.log(res)
-      })
+      document.querySelector('#raven-history-saved-items').redditHistoryToken = getCookie(TOKEN_HISTORY_KEY);
+      document.querySelector('#raven-history-saved-items').username = getCookie(USERNAME_KEY);
     }
   }
 
@@ -50,22 +46,19 @@ class Helpers {
   }
 
   static testHistoryUI() {
-    document.querySelector('#raven-history-saved-items').savedItems = [{
-      "subreddit": "rubyonrails",
-      "title": "Ryan Bates (Railscasts) has surfaced!",
-      "link_flair_text": null,
-      "url": "https://www.reddit.com/r/rubyonrails/comments/11jkghr/ryan_bates_railscasts_has_surfaced/"
-    }, {
-      "subreddit": "redditdev",
-      "title": "How to do a reddit search using API ? Not a subreddit search",
-      "link_flair_text": "Reddit API",
-      "url": "https://www.reddit.com/r/redditdev/comments/z10wzz/how_to_do_a_reddit_search_using_api_not_a/"
-    }, {
-      "subreddit": "CharruaDevs",
-      "title": "Layoffs, recesión? Despidos masivos en ENDAVA? Escucharon algo?",
-      "link_flair_text": "Opinión/Debate",
-      "url": "https://www.reddit.com/r/CharruaDevs/comments/10ytwbc/layoffs_recesión_despidos_masivos_en_endava/"
-    }];
+    document.querySelector('#raven-history-saved-items').savedItems = {
+      "rubyonrails": [{
+        "subreddit": "rubyonrails",
+        "title": "Ryan Bates (Railscasts) has surfaced!",
+        "link_flair_text": null,
+        "url": "https://www.reddit.com/r/rubyonrails/comments/11jkghr/ryan_bates_railscasts_has_surfaced/"
+      }, {
+        "subreddit": "rubyonrails",
+        "title": "Any of you Ruby on Rails developers looking for a job?",
+        "link_flair_text": null,
+        "url": "https://www.reddit.com/r/rubyonrails/comments/ykj73p/any_of_you_ruby_on_rails_developers_looking_for_a/"
+      }]
+    }
   }
 }
 
